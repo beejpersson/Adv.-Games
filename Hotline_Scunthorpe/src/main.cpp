224d33f7e0be0f74ad2522040d72942396899748
Beej Persson <beejpersson@btinternet.com>
2017-09-08 17:04:07 +0100

    Renamed project. Achieved proportional scaling of new player sprite dependant on resolution. Applied position ratio of player to enable player to remain in same position on screen when resolution changes.

--- Hotline_Scunthorpe/src/main.cpp
+++ Hotline_Scunthorpe/src/main.cpp
@@ -31,7 +31,7 @@ enum class GameStates { START, MENU, OPTIONS, LEVEL_1, LEVEL_2 };
 // Method to load in required textures
 void Load() {
   
-  if (!playerTexture.loadFromFile("res/img/spaceship1.png")) {
+  if (!playerTexture.loadFromFile("res/img/player.png")) {
     throw std::invalid_argument("Loading error!");
   }
 	
@@ -46,10 +46,6 @@ void Load() {
 	if (!cursorTexture.loadFromFile("res/img/cursor.png")) {
 		throw std::invalid_argument("Loading error!");
 	}
-
-  // Setting ship's initial position
-  //sprite.setPosition(100, 200);
-
 }
 
 // Rescale all sprites when window resolution is changed
@@ -103,8 +99,27 @@ void RescaleOptions(RenderWindow &window) {
 	optionsBackButton.setFillColor(Color::Magenta);
 }
 
+Vector2f CurrentPlayerPosition(RenderWindow &window) {
+	Vector2f playerPosRatio = Vector2f(float(playerSprite.getPosition().x) / window.getSize().x, float(playerSprite.getPosition().y) / window.getSize().y);
+	std::cout << "\n Ratio x: " << playerPosRatio.x << " \t Ratio y: " << playerPosRatio.y << " \t Window res: " << window.getSize().x << " x " << window.getSize().y << "\t x pos: " << playerSprite.getPosition().x << "\t y pos: " << playerSprite.getPosition().y;
+	return playerPosRatio;
+}
+
+void RepositionPlayer(RenderWindow &window, Vector2f playerPosRatio) {
+	playerSprite.setPosition(Vector2f(float(window.getSize().x) * playerPosRatio.x, float(window.getSize().y) * playerPosRatio.y));
+	std::cout << "\n x pos = " << playerSprite.getPosition().x << " ---- y pos = " << playerSprite.getPosition().y;
+}
+
+void RescaleLevel1(RenderWindow &window) {
+	// Set sprite textures and positons **NEEDS UPDATING**
+	playerSprite.setTexture(playerTexture);
+	playerSprite.setOrigin((playerTexture.getSize().x / 2), (playerTexture.getSize().y / 2));
+	playerSprite.setScale(Vector2f(float(window.getSize().x) / 3840.f, float(window.getSize().y) / 2160.f));
+	
+} 
+
 // Method to use time to continuously update the game
-void Update(Vector2f &mousePos) {
+void Update(Vector2f &mousePos, RenderWindow &window) {
   static sf::Clock clock;
   float dt = clock.restart().asSeconds();
   Vector2f move;
@@ -124,8 +139,8 @@ void Update(Vector2f &mousePos) {
 
 	Vector2f playerPos = playerSprite.getPosition();
 	float ang = atan2((playerPos.y - mousePos.y), (playerPos.x - mousePos.x)) * (180 / M_PI);
-	playerSprite.setRotation(ang-90);
-  playerSprite.move(move*300.0f*dt);
+	playerSprite.setRotation(ang+180);
+  playerSprite.move(move * float(window.getSize().x) * 300.f / 1920.f * dt);
 
 	cursorSprite.setPosition(mousePos.x - cursorSprite.getGlobalBounds().width / 2, mousePos.y - cursorSprite.getGlobalBounds().height / 2);
 }
@@ -153,17 +168,13 @@ int main() {
     return 1;
   }
 
-	// Set sprite textures and positons **NEEDS UPDATING**
-  playerSprite.setTexture(playerTexture);
-	playerSprite.setOrigin((playerTexture.getSize().x / 2), (playerTexture.getSize().y / 2));
-	playerSprite.setScale(Vector2f(2.f, 2.f));
-
 	// Game running while loop
   while (window.isOpen()) {
 		window.setMouseCursorVisible(false);
 
 		int mouseX = Mouse::getPosition(window).x;
 		int mouseY = Mouse::getPosition(window).y;
+		Vector2f playerPosRatio;
 
 
 		// Game state switches with each state's running methods within
@@ -171,7 +182,7 @@ int main() {
 			case GameStates::START:
 				window.clear();
 				RescaleStart(window);
-				Update(Vector2f(mouseX,mouseY));
+				Update(Vector2f(mouseX,mouseY), window);
 				RenderStart(window);
 				window.display();
 
@@ -215,10 +226,12 @@ int main() {
 			case GameStates::OPTIONS:
 				window.clear();
 				RescaleOptions(window);
-				Update(Vector2f(mouseX, mouseY));
+				Update(Vector2f(mouseX, mouseY), window);
 				cursorSprite.setPosition(mouseX - cursorSprite.getGlobalBounds().width / 2, mouseY - cursorSprite.getGlobalBounds().height / 2);
 				RenderOptions(window);
 				window.display();
+
+				playerPosRatio = CurrentPlayerPosition(window);
 
 				Event optionsEvent;
 				while (window.pollEvent(optionsEvent)) {
@@ -228,27 +241,25 @@ int main() {
 					else if (optionsEvent.type == Event::MouseButtonPressed) {
 						// Choose resolution and windowed or fullscreen
 						if (res1080Button.getGlobalBounds().contains(mouseX, mouseY)) {
-							//window.setSize(Vector2u(1920, 1080));
-							//window.setPosition(Vector2i(0, 0));
 							window.create(sf::VideoMode(1920, 1080), "Hotline Scunthorpe", Style::Titlebar | Style::Close);
-							//View view(FloatRect(0, 0, 1000, 1000));
-							//window.setView(view);
+							RepositionPlayer(window, playerPosRatio);
+
 						}
 						else if (res900Button.getGlobalBounds().contains(mouseX, mouseY)) {
-							//window.setSize(sf::Vector2u(1600, 900));
-							//window.setPosition(Vector2i(160, 90));
 							window.create(sf::VideoMode(1600, 900), "Hotline Scunthorpe", Style::Titlebar | Style::Close);
+							RepositionPlayer(window, playerPosRatio);
 						}
 						else if (res720Button.getGlobalBounds().contains(mouseX, mouseY)) {
-							//window.setSize(sf::Vector2u(1280, 720));
-							//window.setPosition(Vector2i(320, 180));
 							window.create(sf::VideoMode(1280, 720), "Hotline Scunthorpe", Style::Titlebar | Style::Close);
+							RepositionPlayer(window, playerPosRatio);
 						}
 						else if (resWindowedButton.getGlobalBounds().contains(mouseX, mouseY)) {
 							window.create(sf::VideoMode(window.getSize().x, window.getSize().y), "Hotline Scunthorpe", Style::Titlebar | Style::Close);
+							RepositionPlayer(window, playerPosRatio);
 						}
 						else if (resFullscreenButton.getGlobalBounds().contains(mouseX, mouseY)) {
 							window.create(sf::VideoMode(window.getSize().x, window.getSize().y), "Hotline Scunthorpe", Style::Fullscreen);
+							RepositionPlayer(window, playerPosRatio);
 						}
 
 						else if (optionsBackButton.getGlobalBounds().contains(mouseX, mouseY)) {
@@ -260,7 +271,8 @@ int main() {
 
 			case GameStates::LEVEL_1:
 				window.clear();
-				Update(Vector2f(mouseX, mouseY));
+				RescaleLevel1(window);
+				Update(Vector2f(mouseX, mouseY), window);
 				cursorSprite.setPosition(mouseX - cursorSprite.getGlobalBounds().width / 2, mouseY - cursorSprite.getGlobalBounds().height / 2);
 				RenderLevel(window);
 				window.display();
